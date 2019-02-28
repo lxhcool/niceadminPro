1ec4dc42a9329f785e8470d892016607e1df5799
lxhcool <1138659665@qq.com>
2019-02-28 15:22:51 +0800

    niceadminpro

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -21,7 +21,6 @@ export default {
   .notfound-wrap {
     width: 100%;
     height: 100%;
-    background: url("../../assets/Error-404.jpg") no-repeat center;
     background-size: cover;
   }
 </style>
